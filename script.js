b3a6b2c607d1cc7f640f08e1f1a205a3daa1c8dc
Jonibek <munirovjonibek@gmail.com>
2022-04-09 10:07:32 +0500

    styling finished

---
+++ script.js
@@ -0,0 +1,26 @@
+let box = document.querySelector(".container");
+fetch("https://api.quran.sutanlab.id/surah")
+  .then((res) => res.json())
+  .then((res) => {
+    let data = res.data;
+    let item;
+    data.forEach((element) => {
+      item = `<div class="card" id ="${element.number}">
+      <div class="item">
+        <div class="num"><p class="number">${element.number}</p></div>
+        <p class="name">${element.name.transliteration.id}</p>
+      </div>
+      <div class="arabic">
+        <p class="arabic__name">${element.name.short}</p>
+        <p class="ayahs">${element.numberOfVerses} Ayahs</p>
+      </div>
+    </div>`;
+      box.insertAdjacentHTML("beforeend", item);
+    });
+  });
+box.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("container")) {
+    console.log(e.target.closest(".card").id);
+    box.style.gridTemplateColumns = "1fr";
+  }
+});
