c2ece6fef7a514262f45a731a9a253406970d3a7
Jonibek <munirovjonibek@gmail.com>
2022-04-09 15:48:00 +0500

    finished project

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let box = document.querySelector(".container");
 let more = document.querySelector(".desc");
+let card = document.querySelector(".card");
+let uzbekText = [];
+let arabText = [];
+let oqilishi = [];
+let audio = [];
+let heading = document.querySelector(".heading");
+let cont = document.querySelector(".cont");
 fetch("https://api.quran.sutanlab.id/surah")
   .then((res) => res.json())
   .then((res) => {
@@ -19,11 +26,75 @@ fetch("https://api.quran.sutanlab.id/surah")
       box.insertAdjacentHTML("beforeend", item);
     });
   });
-box.addEventListener("click", function (e) {
-  if (!e.target.classList.contains("container")) {
-    console.log(e.target.closest(".card").id);
-    box.classList.add("click");
-    more.classList.remove("hidden");
-    document.body.style.padding = "0 2rem";
+let getText = async function (id) {
+  await fetch(`https://api.quran.sutanlab.id/surah/${id}`)
+    .then((res) => res.json())
+    .then((res) => {
+      res.data.verses.forEach((el) => {
+        arabText.push(el.text.arab);
+        oqilishi.push(el.text.transliteration.en);
+        audio.push(el.audio.primary);
+      });
+    });
+};
+box.addEventListener("click", async function (e) {
+  uzbekText = [];
+  arabText = [];
+  oqilishi = [];
+  audio = [];
+  let parent = e.target.closest(".card");
+  if (!parent) return;
+  let text = parent.querySelector(".name").textContent;
+
+  document.querySelector(".heading").textContent = text;
+
+  id = e.target.closest(".card").id;
+  box.classList.add("click");
+  more.classList.remove("hidden");
+  document.body.style.paddingLeft = "2rem";
+
+  // console.log(e.target.textContent);
+  await getUzText(id);
+  await getText(id);
+  console.log(audio);
+  cont.innerHTML = "";
+  for (let i = 0; i < uzbekText.length; i++) {
+    render(arabText[i], uzbekText[i], oqilishi[i], audio[i]);
   }
 });
+
+let getUzText = async function (id) {
+  await fetch(
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions/uzb-alaaudeenmansou.json"
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      res.quran.forEach((element) => {
+        if (element.chapter == id) {
+          uzbekText.push(element.text);
+        }
+      });
+    });
+};
+
+let render = function (arab, uzbek, oqilishi, audio) {
+  let item = `
+  <div class="desc-item">
+  <div class="control"></div>
+  <div class="arabic-text">
+  ${arab}
+  </div>
+  <div class="oqilishi-text">
+ ${oqilishi}
+</div>
+  <div class="uzbek-text">
+   ${uzbek}
+  </div>
+  <audio
+  class="audio-player"
+  controls
+  src="${audio}"
+></audio>
+</div>`;
+  cont.insertAdjacentHTML("beforeend", item);
+};
